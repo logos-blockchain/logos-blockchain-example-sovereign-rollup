e11e1d8fe359229f89548e8fa5f54b3d3297f726
kashepavadan <dkashepava@gmail.com>
2025-10-24 12:05:24 -0400

    minor fix

--- lightnode/src/lib.rs
+++ lightnode/src/lib.rs
@@ -8,4 +8,10 @@ use serde::{Deserialize, Serialize};
 use anyhow::Result;
 use tracing::{debug, info};
 
+use futures::Stream;
+
+pub mod da;
+pub mod nomos;
+pub mod proofcheck;
+
 // Tutorial code
